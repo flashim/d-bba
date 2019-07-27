cb2fa2ce8922433ee742713c249d0e78379ed8f7
Ashim Saha <flashim@gmail.com>
2019-07-27 11:06:02 +0530

    u

---
+++ precache-manifest.677509e93f16d764d078150c8e9136ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04199de5a8aae5f7949bdb47b73ea11c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "55ff63f6d511e815fe0b",
+    "url": "./static/css/main.bd3340e2.chunk.css"
+  },
+  {
+    "revision": "5e272975d480246fe7e1",
+    "url": "./static/js/2.35c348d6.chunk.js"
+  },
+  {
+    "revision": "55ff63f6d511e815fe0b",
+    "url": "./static/js/main.9f8420ee.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
